741ff6fafa4764fe316520fc13935484993aa931
chno <ctnowak65@gmail.com>
2023-12-22 14:57:15 -0500

    [IMP] Added an Improved GUI and an example .desktop. This is the completed program for now.

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-import tkinter as tk
+#!/usr/bin/env python3
+
+from tkinter import *
 from tkinter import ttk, messagebox
 from main import get_supported_currencies, get_exchange_rates
+
+def exit():
+    root.destroy()
 
 def convert_currency():
     source = source_currency_var.get().upper()
@@ -22,31 +27,48 @@ def convert_currency():
     else:
         messagebox.showerror("Conversion Error", "Failed to convert currencies.")
 
-root = tk.Tk()
+#Setting up the main application window.
+root = Tk()
 root.title("Currency Converter")
 
-source_currency_var = tk.StringVar()
-target_currency_var = tk.StringVar()
-amount_var = tk.StringVar()
-result_var = tk.StringVar()
 
-ttk.Label(root, text="Source Currency:").pack()
-source_currency_entry = ttk.Entry(root, textvariable=source_currency_var)
-source_currency_entry.pack()
+#Creating the content frame
+mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
 
-ttk.Label(root, text="Target Currency:").pack()
-target_currency_entry = ttk.Entry(root, textvariable=target_currency_var)
-target_currency_entry.pack()
+source_currency_var = StringVar()
+source_currency_var_entry = ttk.Entry(mainframe, width=7, textvariable=source_currency_var)
+source_currency_var_entry.grid(column=2, row=1, sticky=(W, E))
 
-ttk.Label(root, text="Amount:").pack()
-amount_entry = ttk.Entry(root, textvariable=amount_var)
-amount_entry.pack()
+target_currency_var = StringVar()
+target_currency_var_entry = ttk.Entry(mainframe, width=7, textvariable=target_currency_var)
+target_currency_var_entry.grid(column=2, row=2, sticky=(W, E))
 
-convert_button = ttk.Button(root, text="Convert", command=convert_currency)
-convert_button.pack()
+amount_var = StringVar()
+amount_var_entry = ttk.Entry(mainframe, width=7, textvariable=amount_var)
+amount_var_entry.grid(column=2, row=3, sticky=(W, E))
 
-result_label = ttk.Label(root, textvariable=result_var)
-result_label.pack()
+
+ttk.Label(mainframe, text="Source Currency").grid(column=3, row=1, sticky=W)
+ttk.Label(mainframe, text="Target Currency").grid(column=3, row=2, sticky=W)
+ttk.Label(mainframe, text="Amount").grid(column=3, row=3, sticky=W)
+
+convert_button = ttk.Button(mainframe, text="Convert", command=convert_currency)
+convert_button.grid(column=2, row=4)
+
+result_var = StringVar()
+result_label = ttk.Label(mainframe, textvariable=result_var)
+result_label.grid(column=2, row=5, sticky=(W, E))
+
+root.bind('<Return>', lambda _: convert_currency())
+root.bind('<Escape>', lambda _: exit())
+
+source_currency_var_entry.focus()
+
+for child in mainframe.winfo_children():
+    child.grid_configure(padx=5, pady=5)
 
 root.mainloop()
 
